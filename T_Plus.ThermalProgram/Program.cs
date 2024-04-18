fe5a2e0058d2a9000af4bcc436ef48f064517838
Ilya <powaliaew.il@yandex.ru>
2024-04-18 15:03:36 +0400

    Добавьте файлы проекта.

---
+++ T_Plus.ThermalProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace T_Plus.ThermalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
